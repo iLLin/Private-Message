79d1831f58df09d7e24dc2cb8dae4be748008082
berdir <berdir>
2010-03-22 07:26:40 +0000

    #303643 by nbz, Berdir | litwol: Fixed CSS namespacing.

--- privatemsg-recipients.tpl.php
+++ privatemsg-recipients.tpl.php
@@ -1,9 +1,9 @@
 <?php
-// $Id: privatemsg-recipients.tpl.php,v 1.4 2009-11-30 17:37:15 berdir Exp $
+// $Id: privatemsg-recipients.tpl.php,v 1.5 2010-03-22 07:26:40 berdir Exp $
 // Each file loads it's own styles because we cant predict which file will be
 // loaded.
 drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-recipients.css');
 ?>
-<div class="message-participants">
+<div class="privatemsg-message-participants">
   <?php print $participants; ?>
 </div>
